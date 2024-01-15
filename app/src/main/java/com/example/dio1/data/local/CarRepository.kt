8ac747e4f37dbd030713b4f2228443d8f3a34b5e
Guilherme <guilherme1984.araujo@gmail.com>
2024-01-15 11:50:25 -0300

    Criando a parte de busca de dados

--- app/src/main/java/com/example/dio1/data/local/CarRepository.kt
+++ app/src/main/java/com/example/dio1/data/local/CarRepository.kt
@@ -2,9 +2,15 @@ package com.example.dio1.data.local
 
 import android.content.ContentValues
 import android.content.Context
+import android.provider.BaseColumns
 import android.util.Log
 import androidx.core.content.ContentProviderCompat.requireContext
 import com.example.dio1.Dominio.Carro
+import com.example.dio1.data.local.CarContract.carEntry.COLUMN_NAME_BATERIA
+import com.example.dio1.data.local.CarContract.carEntry.COLUMN_NAME_POTENCIA
+import com.example.dio1.data.local.CarContract.carEntry.COLUMN_NAME_PRECO
+import com.example.dio1.data.local.CarContract.carEntry.COLUMN_NAME_RECARGA
+import com.example.dio1.data.local.CarContract.carEntry.COLUMN_NAME_URL_PHOTO
 
 class CarRepository (private val context: Context) {
 
@@ -32,5 +38,29 @@ class CarRepository (private val context: Context) {
           }
        }
         return isSaved
+    }
+
+    fun findCarById(id: Int){
+        val dbHelper = CarsDbHelper(context)
+        val db = dbHelper.writableDatabase
+
+        //Listam as colunas a serrem exibidas no resultado da query
+        val columns = arrayOf(
+            BaseColumns._ID,
+            COLUMN_NAME_PRECO,
+            COLUMN_NAME_BATERIA,
+            COLUMN_NAME_POTENCIA,
+            COLUMN_NAME_RECARGA,
+            COLUMN_NAME_URL_PHOTO)
+
+        val filter = "${BaseColumns._ID} = ?"
+        val filterValues = arrayOf(id.toString())
+
+        val cursor = db.query(
+            CarContract.carEntry.TABLE_NAME,
+            columns,
+            filter,
+            filterValues
+        )
     }
 }
